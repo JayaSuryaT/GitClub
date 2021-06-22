8270d9a2175cf0368739a0054a0a41f382aa5ea4
JayaSuryaT <jayasurya.dc@gmail.com>
2021-06-22 19:15:27 +0530

    Feat : Brought in caching client.

---
+++ data/src/main/java/com/digitalcrafts/gitClub/data/internal/cache/CacheClient.kt
@@ -0,0 +1,23 @@
+package com.digitalcrafts.gitClub.data.internal.cache
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.room.Room
+
+@SuppressLint("StaticFieldLeak")
+internal object CacheClient {
+
+    private const val ROOM_DB_NAME: String = "gitClubDb"
+
+    private var mDb: GitClubDatabase? = null
+
+    internal val dataBase: GitClubDatabase
+        get() = mDb ?: throw IllegalStateException("Database not initialized.")
+
+
+    internal fun initialize(context: Context) {
+        mDb = Room
+            .databaseBuilder(context, GitClubDatabase::class.java, ROOM_DB_NAME)
+            .build()
+    }
+}
